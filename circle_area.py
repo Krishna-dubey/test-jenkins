00491319ffd77d66cf84441b3dad29642ff7d716
Krishna Dubey <krishnadubey1997@gmail.com>
2020-08-19 00:21:12 +0530

    Add files via upload

--- circle_area.py
+++ circle_area.py
@@ -1,8 +1,12 @@
+'''Importing constant pi from math module
+   to be used in area of circle formula'''
 from math import pi
 
-def circleArea(radius):
-	if type(radius) not in [int,float]:
-		raise TypeError("The radius must be non-negative real number")
-	if radius<0:
-		raise ValueError("The radius cannot be negative") 
-	return pi*(radius*radius)
+def circle_area(radius):
+    '''Defining function which returns area of circle
+   by taking radius as input'''
+    if type(radius) not in [int, float]:
+        raise TypeError("The radius must be non-negative real number")
+    if radius < 0:
+        raise ValueError("The radius cannot be negative")
+    return pi*(radius*radius)
--- test_circle_area.py
+++ test_circle_area.py
@@ -1,21 +1,23 @@
+'''Importing required module'''
 import unittest
-from circle_area import circleArea
 from math import pi
+from circle_area import circle_area
 
 class TestCircleArea(unittest.TestCase):
-	def test_area(self):
-		#Testing area when radius >= 0
-		self.assertAlmostEqual(circleArea(1),pi)
-		self.assertAlmostEqual(circleArea(0),0)
-		self.assertAlmostEqual(circleArea(3.3),pi*3.3*3.3)
+    '''Class to unit test circle_area function'''
+    def test_area(self):
+        '''Testing area when radius >= 0'''
+        self.assertAlmostEqual(circle_area(1), pi)
+        self.assertAlmostEqual(circle_area(0), 0)
+        self.assertAlmostEqual(circle_area(3.3), pi*3.3*3.3)
 
-	def test_radius_values(self):
-		#Raise value error if neccessary
-		self.assertRaises(ValueError,circleArea, -2)
+    def test_radius_values(self):
+        '''Raise value error if neccessary'''
+        self.assertRaises(ValueError, circle_area, -2)
 
-	def test_radius_values_types(self):
-		#Raise type error if neccessary
-		self.assertRaises(TypeError,circleArea, "r")
-		self.assertRaises(TypeError,circleArea, True)
-		self.assertRaises(TypeError,circleArea, 3+5j)
-
+    def test_radius_values_types(self):
+        '''Raise type error if neccessary'''
+        self.assertRaises(TypeError, circle_area, "r")
+        self.assertRaises(TypeError, circle_area, True)
+        self.assertRaises(TypeError, circle_area, 3+5j)
+		
